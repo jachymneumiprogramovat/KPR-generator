82580ab00e2d352629d8c2195a1ba278a0151808
Jachym L <jachym.lowenhoffer@gmail.com>
2023-05-24 17:46:58 +0200

    file for testing functions in the main kpr file

---
+++ testing.py
@@ -0,0 +1,15 @@
+from kpr import idlist,body,primky,bod,primka
+from kpr import pridej_bod,pridej_primku,protahni_primku,print_kprinfo
+
+for it in range(5):
+    body.append(bod([],idlist[it]))
+
+pridej_primku([body[0],body[1],body[2]])
+pridej_primku([body[1],body[3]])
+
+protahni_primku(primky[0],body[4])
+protahni_primku(primky[1],body[4])
+
+
+print_kprinfo()
+
