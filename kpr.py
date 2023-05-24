b322f3ff217064dfc73c96a4bf4ce0f9a4af5076
Jachym L <jachym.lowenhoffer@gmail.com>
2023-05-24 16:39:58 +0200

    finished basic functions and aded some doc strings

--- kpr.py
+++ kpr.py
@@ -14,7 +14,9 @@ class primka:
                 if element in primka.elements:
                     self.protina.append(primka)
     
-    def satisfy_line_axiom(self)->None:
+    def satisfy_line_axiom(self)->bool:
+        """Vraci true, když line axiom platí pro danou přímku."""
+        satisfies = True
         for primka in primky:
             if primka in self.protina: continue
             for bod in primka.elements:
@@ -22,9 +24,11 @@ class primka:
                     if line in self.protina:
                         break
                 protahni_primku(self,bod)
-                self.update_protina()
+                satisfies = False
                 break
             pridej_bod(self,primka)
+            satisfies=False
+        return satisfies
 
 class bod:
     def __init__(self,protina:list[primka],id:str) -> None:
@@ -39,18 +43,23 @@ class bod:
                     self.spojesny_s.append(bod)
 
     def satisfy_vertex_axiom(self):
+        """Pocitá s tím, že line axiom je splněný. Vraci true, když je vertex axiom splěný pro daný bod."""
         for bod in body:
             if bod in self.spojesny_s: continue
+            pridej_primku(self,bod)
+
             
 
 
 def protahni_primku(primka:primka,bod:bod)->None:
+    """Přidá do přímky bod a zároveň updatuje všechny seznami"""
     primka.elements.append(bod)
     primka.protina.append([x for x in bod.nalezi])
     bod.nalezi.append(primka)
     bod.spojesny_s.append([x for x in primka.elements])
 
 def pridej_primku(body:list[bod])->None:
+    """Vytvoří novou přímku z bodů. Zjistí s jakými všemi přímkami se protíná"""
     nprimka= primka(body)
     for bod in body:
         bod.nalezi.append(nprimka)
@@ -59,6 +68,7 @@ def pridej_primku(body:list[bod])->None:
 
 
 def pridej_bod(primka1:primka,primka2:primka)->None:
+    """Vytovří nový bod jako průnik dvou přímek ty upraví jako, že se protínají."""
     nbod = bod([primka1,primka2],idlist[len(body)])
     primka1.elements.append(nbod)
     primka2.elements.append(nbod)
@@ -66,13 +76,6 @@ def pridej_bod(primka1:primka,primka2:primka)->None:
     primka2.protina.append(primka1)
     body.append(nbod)
 
-
-#delete later meaby useles
-def disjunkt_primky(primka1:primka,primka2:primka)->bool:
-    for element in primka1.elements:
-        if element in primka2.elements:
-            return False
-    return True
 
 
 
