89d2c0611dd630b1ae8ada5920bea0a3e9e4086e
Jachym L <jachym.lowenhoffer@gmail.com>
2023-05-25 14:42:40 +0200

    spravena funkce pridej bod

--- testing.py
+++ testing.py
@@ -1,13 +1,14 @@
 from kpr import idlist,body,primky,bod,primka
 from kpr import pridej_bod,pridej_primku,protahni_primku,print_kprinfo
 
-for it in range(5):
+for it in range(4):
     body.append(bod([],idlist[it]))
 
-pridej_primku([body[0],body[1],body[2]])
-pridej_primku([body[1],body[4]])
+pridej_primku([body[0],body[1]])
+pridej_primku([body[0],body[2]])
+pridej_primku([body[2],body[3]])
 
-protahni_primku(primky[1],body[3])
+pridej_bod(primky[0],primky[2])
 
 
 print_kprinfo()
